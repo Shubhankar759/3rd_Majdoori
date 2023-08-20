1c3915923a209d12b8cd130959714929c7e9b275
shubhankar75 <slwarkade@gcoen.ac.in>
2023-08-20 14:43:09 +0530

    First

---
+++ Evenodd.java
@@ -0,0 +1,9 @@
+public class Evenodd {
+    public static void main(String args[]){
+        int x=750;
+        if(x%2==0)
+        System.out.println("Even");
+        else
+        System.out.println("Odd");
+    }
+}
