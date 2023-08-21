3d79dc7b6c4bf6f171fb954d932419e4a0bba139
shubhankar75 <slwarkade@gcoen.ac.in>
2023-08-21 11:22:31 +0530

    initial

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Add {
+    public static void main(String args[]){
+    Scanner n = new Scanner(System.in);
+    int x = n.nextInt();
+    int y = n.nextInt();
+    int c=x+y;
+    System.out.println("the additon of number is "+c);
+    }
+}
