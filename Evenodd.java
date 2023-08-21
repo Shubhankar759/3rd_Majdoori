865383ad4fa713e992350514311f58919d1421d8
Shubhankar759 <shubhankarwarkade@gmail.com>
2023-08-21 19:27:41 +0530

    Delected portion

--- Evenodd.java
+++
@@ -1,9 +0,0 @@
-public class Evenodd {
-    public static void main(String args[]){
-        int x=750;
-        if(x%2==0)
-        System.out.println("Even");
-        else
-        System.out.println("Odd");
-    }
-}
--- Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello{
-    public static void main (String args[]){
-        int x=10;
-        int y= 10/5;
-        System.out.println("x is "+x+" y is "+y);
-    }
-}
