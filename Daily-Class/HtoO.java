20cda6eb787f339b90fbebb827ea5924848f3710
Shubhankar759 <shubhankarwarkade@gmail.com>
2023-08-24 11:10:22 +0530

    24/08/2023 Q4

---
+++ Daily-Class/HtoO.java
@@ -0,0 +1,11 @@
+public class HtoO {
+    public static void main(String[] args) {
+        int x=100;
+        while (x!=0) {
+            if(x%2==0||x%5==0){
+                System.out.println(x);
+            }
+            x--;
+        }
+    }
+}
