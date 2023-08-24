1bc580e888216885559cfea4f5adffee49a1143a
Shubhankar759 <shubhankarwarkade@gmail.com>
2023-08-24 11:10:02 +0530

    24/08/2023 Q3

---
+++ Daily-Class/Reversenumber.java
@@ -0,0 +1,10 @@
+//WAP print number in reverse using while loop
+public class Reversenumber {
+    public static void main(String[] args) {
+        int x=10;
+        while(x!=0){
+            System.out.println(x);
+            x--;
+        }
+    }
+}
